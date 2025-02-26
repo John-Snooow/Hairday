a242a1fcb59d763fb373ea81862665bf0d43e444
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-26 17:45:27 -0300

    Criando load para arregamento dos orarios

---
+++ src/modules/load.js
@@ -0,0 +1,6 @@
+// arquivo de carregamento de paginas baseado nos horarios disponiveis
+
+// Carrega o DOM
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM carregado")
+})
