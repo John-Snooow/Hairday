4d917adb4edda22270e68e38bcadac052f8ca53d
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-03 13:34:13 -0300

    Update hours-click.js

--- src/modules/form/hours-click.js
+++ src/modules/form/hours-click.js
@@ -1,15 +1,15 @@
-export function hoursClick(){
-    const hours = document.querySelectorAll(".hour-available")
-    
-    hours.forEach((available) => {
-        available.addEventListener("click", (selected) => {
-            // remove a classe hour-selected de todas as li não selecionadas
-            hours.forEach((hour) => {
-                hour.classList.remove("hour-selected")
-            })
+export function hoursClick() {
+  const hours = document.querySelectorAll(".hour-available");
 
-            // adiciona a classe na lista
-            selected.target.classList.add("hour-selected")
-        })
-    })
+  hours.forEach((available) => {
+    available.addEventListener("click", (selected) => {
+      // remove a classe hour-selected de todas as li não selecionadas
+      hours.forEach((hour) => {
+        hour.classList.remove("hour-selected");
+      });
+
+      // adiciona a classe na lista
+      selected.target.classList.add("hour-selected");
+    });
+  });
 }
