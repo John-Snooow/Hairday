bce5f702cf00fa0782922dd3a7ef153fd48199f5
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-08 22:11:11 -0300

    Buscando agendamento

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,12 +1,15 @@
+import {scheduleFetchByDay} from "../../services/schedule-fetch-by-day.js"
 import {hoursLoad} from "../form/hours-load.js"
 
 //seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay(){
+export async function schedulesDay(){
     //Obtem a data do input
     const date = selectedDate.value
 
+    //Busca na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({date})
 
     // Renderiza os horarios disponiveis
     hoursLoad({date})
